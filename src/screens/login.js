dbf64b10f4ac85b25fea8e5949ce4fc663a31a1e
gauravguptabits <2019ad04038@wilp.bits-pilani.ac.in>
2022-02-25 19:57:18 +0530

    Added login page

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Row, Col } from "react-bootstrap";
+import { Button, Row, Col, Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { getUserAction } from "../actions/userAction";
 import { withRouter } from "react-router";
@@ -44,17 +44,22 @@ class LandingPage extends React.Component {
 
     render() {
         return (
-            <Row>
-                <Col>
-                    <Button variant="secondary" 
-                            onClick={e => this.onEnterAsGuestClicked(e)}
-                    >Enter as Guest Login</Button>
-                </Col>
-                <Col>
-                    { this.state.date.toISOString() }
-                    { this.state.user.name }
-                </Col>
-            </Row>
+            <div className="Login">
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={(e) => this.onEnterAsGuestClicked(e)}>
+                        Login
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
